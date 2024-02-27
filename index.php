1efac4c967105f071f2291efc22d349c50198b0e
Federico Letizia <federicoletizia12@gmail.com>
2024-02-27 19:26:14 +0100

    php primo

---
+++ index.php
@@ -0,0 +1,49 @@
+<?php
+
+$Numero = 89;
+
+$Numero2 = 8.5;
+
+$testo = 'ciao';
+
+$booleano = true;
+
+var_dump($Numero ." ". $Numero2 ." ". $testo ." ". $booleano);
+
+
+const Numero = 89;
+
+const Numero2 = 8.5;
+
+const testo = 'ciao';
+
+const booleano = true;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
